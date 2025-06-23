4a6aa052d13cc34bc1dfcd54fdc1164fd2322e3a
Shivam verma <vermashivam8868@gmail.com>
2025-06-23 23:20:18 +0530

    navigation scheme utilize

---
+++ src/screens/dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+  return (
+    <div style={{ display: 'flex', backgroundColor: 'red', flex: 1 }}>
+      <p>my name is</p>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/screens/favourite.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Favourite() {
+    return (
+        <div style={{ display: 'flex', backgroundColor: 'red', flex: 1 }}>
+            <p>favourite screen</p>
+        </div>
+    )
+}
+
+export default Favourite;
